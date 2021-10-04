2d2e596e43e2b33ec6805025c161a724749b7f80
iravishankarpal <iravishankarpal@gmail.com>
2021-10-04 19:09:36 +0000

    Updates

---
+++ precache-manifest.83809d489d9355a226a9dcc65084b18a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2718d667b3fc9fce706f4eda947d0279",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "1d2da374cfada774e970",
+    "url": "/react-covid-tracker/static/css/2.7d011da2.chunk.css"
+  },
+  {
+    "revision": "9365e6ee9b83f4f57428",
+    "url": "/react-covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "1d2da374cfada774e970",
+    "url": "/react-covid-tracker/static/js/2.9bfe73f9.chunk.js"
+  },
+  {
+    "revision": "2eec0a263ef1f7393322e8c89c5ff82a",
+    "url": "/react-covid-tracker/static/js/2.9bfe73f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9365e6ee9b83f4f57428",
+    "url": "/react-covid-tracker/static/js/main.58615b41.chunk.js"
+  },
+  {
+    "revision": "261fd4b71819be31f984",
+    "url": "/react-covid-tracker/static/js/runtime-main.803e8507.js"
+  }
+]);
